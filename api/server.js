b845e0b48b57a82bff112fe0a7c1d6c695abe4d8
Jimmique <jimmique88@gmail.com>
2020-02-26 23:16:32 -0800

    Set up Postgres to persist data

---
+++ api/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+
+const authenticate = require('./../auth/authenticate-middleware.js');
+const authRouter = require('./../routers/auth-router.js');
+const usersRouter = require('./../routers/users-router.js');
+//const recipesRouter = require('./../routers/recipes-router.js');
+
+const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+server.use("/api/auth", authRouter);
+server.use("/api/users", usersRouter);
+
+server.get("/", (req, res) => {
+    res.status(200).json({ api: "Server is live" });
+});
+
+function errorHandler(err, req, res, next) {
+    res.status(err.status || 500).json({
+        message: err.message || 'Internal Server Error'
+    });
+}
+
+server.use(erroHandler)
+
+module.exports = server;
